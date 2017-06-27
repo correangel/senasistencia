6eba341a8870356b757fffc418d95abdb5ba735d
david rugeles <rugeles.david@gmail.com>
2017-06-27 14:53:09 -0500

    plantilla

--- plantilla-control.php
+++ plantilla-control.php
@@ -12,7 +12,7 @@ if (isset($_REQUEST['ac']))
       $xclase->__SET('xatributo:clase',$_REQUEST['XXCAMPO']);//XXCAMPO corresponde al nombre que tiene el <input> en la vista
       $xclase->__SET('xatributoforaneo',$_REQUEST['xxcampoforaneo']);//xxcampoforaneo corresponde al nombre que tiene el <select> de la vista
       $modelo->crear($xclase);//reemplazar $xclase por lavariable con la clase
-      header("Location : xnombreArchivo-vista.php");//reenplazar por  xnombreArchivo el nombre del archivo de la vista
+      header("Location: xnombreArchivo-vista.php");//reenplazar por  xnombreArchivo el nombre del archivo de la vista
 
     break;
     case 'actualizar':
@@ -20,16 +20,15 @@ if (isset($_REQUEST['ac']))
     $xclase->__SET('xatributo:clase',$_REQUEST['XXCAMPO']);//XXCAMPO corresponde al nombre que tiene el <input> en la vista
     $xclase->__SET('xatributoforaneo',$_REQUEST['xxcampoforaneo']);//xxcampoforaneo corresponde al nombre que tiene el <select> de la vista
     $modelo->actualizar($xclase);//reemplazar $xclase por lavariable con la clase
-    header("Location : xnombreArchivo-vista.php");//reenplazar por  xnombreArchivo el nombre del archivo de la vista
+    header("Location: xnombreArchivo-vista.php");//reenplazar por  xnombreArchivo el nombre del archivo de la vista
 
-      header("Location : xnombreArchivo-vista.php");
     break;
     case 'editar':
       $xclase = $modelo->editar($_REQUEST['id']);//la variable que contiene la clase sera igual a lo que devuelve la funcion del modelo llamada editar
     break;
     case 'eliminar':
       $modelo->eliminar($_REQUEST['id'])
-      header("Location : xnombreArchivo-vista.php");
+      header("Location: xnombreArchivo-vista.php");
     break;
     default:
 
